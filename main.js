72d49cfcc5da8ecd4ca33104d5d28bd6d76af5d5
jimdmm <jimdouglasmmferreira@gmail.com>
2022-12-22 18:20:21 -0300

    #animacaoScroll

--- main.js
+++ main.js
@@ -62,3 +62,25 @@ function initScrollSuave() {
     })
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+
+    const sections = document.querySelectorAll('.js-scroll');
+    
+    if (sections.length) {
+        const windowMetade = window.innerHeight * 0.5;
+    
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const sectionIsVisible = (sectionTop - windowMetade) < 0;
+                if (sectionIsVisible) {
+                    section.classList.add('ativo')
+                }
+            })
+        }
+    
+        window.addEventListener('scroll', animaScroll);
+    }
+}
+initAnimacaoScroll();
