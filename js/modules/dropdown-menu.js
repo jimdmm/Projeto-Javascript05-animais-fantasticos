8b22fb73cadc0f44ad61fe5e19eb206ca8fea48d
jim <jimdouglasmmferreira@gmail.com>
2023-01-23 21:02:44 -0300

    #dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,26 +1,26 @@
 export default function initDropdownMenu() {
+    
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-
-}
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick);
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick);
+        })
     })
-})
-
-function handleClick(e) {
-    e.preventDefault();
-    this.classList.toggle('active');
-    outsideClick();
-}
-
-function outsideClick(callback) {
-    const html = document.documentElement;
-    html.addEventListener('click', handleOutsideClick);
-    function handleOutsideClick(e) {
-        
+    
+    function handleClick(e) {
+        e.preventDefault();
+        this.classList.toggle('active');
+        outsideClick();
     }
+    
+    function outsideClick(callback) {
+        const html = document.documentElement;
+        html.addEventListener('click', handleOutsideClick);
+        function handleOutsideClick(e) {
+            
+        }
+    }
+
 }
+
