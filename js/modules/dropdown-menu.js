77948a3ed08bedb7066bcbe4e723eb3563705a5f
jim <jimdouglasmmferreira@gmail.com>
2023-01-29 18:04:42 -0300

    Dropdown pt2

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,26 +1,37 @@
 export default function initDropdownMenu() {
-    
-    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-    dropdownMenus.forEach(menu => {
-        ['touchstart', 'click'].forEach(userEvent => {
-            menu.addEventListener(userEvent, handleClick);
-        })
-    })
-    
-    function handleClick(e) {
-        e.preventDefault();
-        this.classList.toggle('active');
-        outsideClick();
-    }
-    
-    function outsideClick(callback) {
-        const html = document.documentElement;
-        html.addEventListener('click', handleOutsideClick);
-        function handleOutsideClick(e) {
-            
-        }
-    }
 
 }
 
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+    })
+})
+
+function handleClick(e) {
+    e.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, () => {
+        this.classList.remove('active');
+    });
+}
+
+function outsideClick(element, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+
+    if (!element.hasAttribute(outside)) {
+        html.addEventListener('click', handleOutsideClick);
+        element.setAttribute(outside, '');
+    }
+    function handleOutsideClick(e) {
+        if (!element.contains(e.target)) {
+            element.removeAttribute(outside);
+            html.removeEventListener('click', handleOutsideClick);
+            callback();
+        }
+    }
+}
